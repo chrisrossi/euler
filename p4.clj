5a0904906533d230ee9d2bb1683a41f763f59986
Chris Rossi <chris@archimedeanco.com>
2013-09-23 13:27:36 -0400

    Problem 4

---
+++ p4.clj
@@ -0,0 +1,13 @@
+(def X 1000)
+
+(def Xs (range (int (* 0.9 X)) X))
+
+(def products 
+  (sort > (map #(apply * %) 
+    (reduce #(conj %1 %2) #{} (for [x Xs y Xs] (sort [x y]))))))
+
+(defn palindrome? [x]
+  (let [s (str x)]
+    (= s (clojure.string/reverse s))))
+
+(println (first (filter palindrome? products)))
