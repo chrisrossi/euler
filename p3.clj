b8e1f7f624241659ffce9dda38463819784adda9
Chris Rossi <chris@archimedeanco.com>
2013-09-22 23:37:39 -0400

    Faster

--- p3.clj
+++ p3.clj
@@ -1,7 +1,10 @@
 (defn factors
   [x]
-  (let [limit (quot x 2)]
-    (map #(quot x %) (filter #(= (mod x %) 0) (take-while #(< % limit) (iterate #(+ % 2) 3))))))
+  (let [limit (java.lang.Math/sqrt x)]
+    (map (fn [y] [y (quot x y)])
+         (filter #(= (mod x %) 0) 
+                 (take-while #(< % limit) (iterate #(+ % 2) 3))))))
+
 
 (defn prime?
   [x]
@@ -11,4 +14,4 @@
     :else (empty? (factors x))))
 
 
-(println (first (filter prime? (factors 600851475143))))
+(println (first (filter prime? (sort > (flatten (factors 600851475143))))))
